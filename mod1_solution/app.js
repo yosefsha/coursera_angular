9c205b7f013aa3fbab268f6c0f7c388c6b965370
Yossi Shachnovsky <yossi@smartbus.co.il>
2017-07-09 15:04:46 +0300

    mod 1

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -15,11 +15,20 @@
     $scope.lunch_message = GetLunchMessage(items_count);
     };
 
+    $scope.lunch_message_style = function(){
+      if ($scope.lunch_message == "Enjoy!"){
+        return "text-success"
+      }
+      if ($scope.lunch_message == "Too much!"){
+        return "text-danger"
+      }
+    };
+
   };
 
 function GetLunchMessage(items_count){
     switch (true) {
-        case (items_count === 0): return "Please enter data first";
+        case (items_count === 0): return "Please enter data first!";
           break;
         case (items_count <= 3): return "Enjoy!"
           break;
@@ -40,5 +49,14 @@ function CountItems(data_string) {
   return data_string.split(",").length
 };
 
+function GetLunchMessageStyle(text){
+  if (text == "Enjoy!"){
+    return "text-success"
+  }
+  if (text == "Too much!"){
+    return "text-dange"
+  }
+
+}
 
 })();

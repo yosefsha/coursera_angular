2f816eb6f67048b34f4248ed2e5bc80139697083
Yossi Shachnovsky <yossi@smartbus.co.il>
2017-07-19 17:42:58 +0300

    dsfadsf

---
+++ mod3_solution/app.js
@@ -0,0 +1,57 @@
+(function(){
+  'use strict;'
+
+  var app = angular.module('NarrowItDownApp', []);
+
+  app.controller('NarrowItDownController', NarrowItDownController);
+  app.service('MenuSearchService', MenuSearchService);
+  app.directive('foundItems', FoundItems);
+  app.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+  NarrowItDownController.$inject = ['$log', 'MenuSearchService'];
+
+  NarrowItDownController = function(MenuSearchService){
+    var vm = this;
+
+    vm.search_txt;
+    vm.found = [];
+
+    vm.getMenueItems = function(){
+      vm.found = MenuSearchService.getMatchedMenuItems(vm.search_txt);
+    };
+
+
+  };
+
+  MenuSearchService.inject = ['$http'];
+
+  MenuSearchService = function($http){
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+        var response = getMenueFromServer();
+        var allMenueItems = response.data;
+        var matched_items = [];
+
+        for (let item in allMenueItems){
+          if (searchTerm in item.name)
+          {
+            matched_items.push(item);
+          }
+        }
+
+        return matched_items;
+    };
+
+    service.getMenueFromServer = function(){
+      var response = $http({
+        method: "GET",
+        url: (ApiBasePath + "/categories.json")
+      });
+
+      return response;
+    };
+
+  };
+
+});

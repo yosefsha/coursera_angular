0de1bc7113874b6aab7ca8942f7c86370dbb214c
Yossi Shachnovsky <yossi@smartbus.co.il>
2017-07-20 15:47:28 +0300

    sfdas

--- mod3_solution/app.js
+++ mod3_solution/app.js
@@ -1,16 +1,16 @@
 (function(){
-  'use strict;'
+'use strict';
+  var ApiBasePath = "https://davids-restaurant.herokuapp.com"
 
   var app = angular.module('NarrowItDownApp', []);
-
   app.controller('NarrowItDownController', NarrowItDownController);
   app.service('MenuSearchService', MenuSearchService);
-  app.directive('foundItems', FoundItems);
-  app.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
-
+  // app.directive('foundItems', FoundItems);
+  // app.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+  //
   NarrowItDownController.$inject = ['$log', 'MenuSearchService'];
 
-  NarrowItDownController = function(MenuSearchService){
+  function NarrowItDownController($log, MenuSearchService){
     var vm = this;
 
     vm.search_txt;
@@ -20,12 +20,11 @@
       vm.found = MenuSearchService.getMatchedMenuItems(vm.search_txt);
     };
 
-
   };
 
   MenuSearchService.inject = ['$http'];
 
-  MenuSearchService = function($http){
+  function MenuSearchService($http){
     var service = this;
 
     service.getMatchedMenuItems = function(searchTerm) {
@@ -43,7 +42,7 @@
         return matched_items;
     };
 
-    service.getMenueFromServer = function(){
+    function getMenueFromServer(){
       var response = $http({
         method: "GET",
         url: (ApiBasePath + "/categories.json")
@@ -54,4 +53,71 @@
 
   };
 
-});
+
+
+
+})();
+
+
+
+
+
+
+//
+// (function(){
+//   'use strict;'
+//
+//   var app = angular.module('NarrowItDownApp', []);
+//
+//   app.controller('NarrowItDownController', NarrowItDownController);
+//   app.service('MenuSearchService', MenuSearchService);
+//   app.directive('foundItems', FoundItems);
+//   app.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+//
+//   NarrowItDownController.$inject = ['$log', 'MenuSearchService'];
+//
+//   NarrowItDownController = function(MenuSearchService){
+//     var vm = this;
+//
+//     vm.search_txt;
+//     vm.found = [];
+//
+//     vm.getMenueItems = function(){
+//       vm.found = MenuSearchService.getMatchedMenuItems(vm.search_txt);
+//     };
+//
+//
+//   };
+//
+//   MenuSearchService.inject = ['$http'];
+//
+//   MenuSearchService = function($http){
+//     var service = this;
+//
+//     service.getMatchedMenuItems = function(searchTerm) {
+//         var response = getMenueFromServer();
+//         var allMenueItems = response.data;
+//         var matched_items = [];
+//
+//         for (let item in allMenueItems){
+//           if (searchTerm in item.name)
+//           {
+//             matched_items.push(item);
+//           }
+//         }
+//
+//         return matched_items;
+//     };
+//
+//     service.getMenueFromServer = function(){
+//       var response = $http({
+//         method: "GET",
+//         url: (ApiBasePath + "/categories.json")
+//       });
+//
+//       return response;
+//     };
+//
+//   };
+//
+// });
